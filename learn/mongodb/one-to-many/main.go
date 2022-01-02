5ece37bc7951ecbf0b04290cbab0021d38997845
Kristopher <34522491+topheruk@users.noreply.github.com>
2022-01-02 11:34:48 +0000

    .

--- learn/mongodb/one-to-many/main.go
+++ learn/mongodb/one-to-many/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -24,7 +24,6 @@ type Episodes struct {
 }
 
 func main() {
-	flag.Parse()
 	if err := run(); err != nil {
 		fmt.Fprintf(os.Stderr, "%v", err)
 		os.Exit(1)
@@ -32,6 +31,27 @@ func main() {
 }
 
 func run() (err error) {
+	cfg, err := env()
+	if err != nil {
+		return
+	}
+
+	fmt.Println(cfg.Foo)
 
 	return
+}
+
+type Config struct {
+	Foo string
+}
+
+func env() (cfg *Config, err error) {
+	err = godotenv.Load("learn/mongodb/one-to-many/.env")
+	if err != nil {
+		return
+	}
+
+	return &Config{
+		Foo: os.Getenv("FOO"),
+	}, nil
 }
