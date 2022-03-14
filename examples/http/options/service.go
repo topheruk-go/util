e9ded3ec286aa251faf2e5eafd3f84984496a53f
topheruk <kristopherab@gmail.com>
2022-03-14 09:47:35 +0000

    clean-up

--- examples/http/options/service.go
+++ examples/http/options/service.go
@@ -10,35 +10,6 @@ type service struct {
 
 type option func(*service)
 
-type builder struct {
-	opts []option
-}
-
-func newBuilder() *builder {
-	b := &builder{
-		make([]option, 0),
-	}
-	return b
-}
-
-func (b *builder) withTimeOut(t time.Duration) *builder {
-	b.opts = append(b.opts, withTimeOut(t))
-	return b
-}
-
-func (b *builder) withMaxConn(m int) *builder {
-	b.opts = append(b.opts, withMaxConn(m))
-	return b
-}
-
-func (b *builder) build() []option {
-	return b.opts
-}
-
-// -- options builder
-// this struct would allow me to do the following:
-// opts := Options().WithTimeout(time_dureation).WithMaxConn(max_conn).Build()
-
 func newService(address string, os ...option) *service {
 	service := &service{
 		address: address,
