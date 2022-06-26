61a1dde68dcedd1c6dd7233b7f9c29d08414b39e
topheruk <kristopherab@gmail.com>
2022-06-26 08:44:05 +0000

    remove uneccessary helper function

--- testing/http.go
+++ testing/http.go
@@ -13,27 +13,22 @@ import (
 
 func HttpService(t *testing.T, srv *httptest.Server, tt []ptth.TestCase) {
 	for _, tc := range tt {
-		t.Run(tc.Name, httpService(srv, tc))
-	}
-}
+		if tc.ContentType == "" {
+			tc.ContentType = "application/json"
+		}
+		if tc.Status == 0 {
+			tc.Status = http.StatusOK
+		}
+		t.Run(tc.Name, func(t *testing.T) {
+			req, err := http.NewRequest(tc.Method, srv.URL+tc.PathName, strings.NewReader(tc.Content))
+			assert.Equal(t, err, nil)
 
-func httpService(srv *httptest.Server, tc ptth.TestCase) func(*testing.T) {
-	if tc.ContentType == "" {
-		tc.ContentType = "application/json"
-	}
-	if tc.Status == 0 {
-		tc.Status = http.StatusOK
-	}
+			req.Header.Add("Content-Type", tc.ContentType)
 
-	return func(t *testing.T) {
-		req, err := http.NewRequest(tc.Method, srv.URL+tc.PathName, strings.NewReader(tc.Content))
-		assert.Equal(t, err, nil)
+			res, err := srv.Client().Do(req)
+			assert.Equal(t, err, nil)
 
-		req.Header.Add("Content-Type", tc.ContentType)
-
-		res, err := srv.Client().Do(req)
-		assert.Equal(t, err, nil)
-
-		assert.Equal(t, res.StatusCode, tc.Status)
+			assert.Equal(t, res.StatusCode, tc.Status)
+		})
 	}
 }
